0fe99c0280fe1e78b11232e9ee0984efbc2a4902
Sai-Raj-Ltd <57390445+Sai-Raj-Ltd@users.noreply.github.com>
2020-01-20 15:11:58 +0530

    Config File

--- hr_ke/models/res_config.py
+++ hr_ke/models/res_config.py
@@ -39,15 +39,12 @@ class KEPayrollSettings(models.TransientModel):
     employer_nssf = fields.Char(
         'Employer NSSF NO.',
         related='company_id.employer_nssf',
-        required=True,
         size=9)
     employer_nhif = fields.Char(
         'Employer NHIF CODE.',
         related='company_id.employer_nhif',
-        required=True,
         size=6)
     employer_kra = fields.Char(
         'Employer KRA PIN.',
         related='company_id.employer_kra',
-        required=True,
         size=11)
